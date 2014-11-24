1fe15a93ff578f4e1054ae62a62751c115ebf078
Rubens Saraiva <rubens.saraiva@gmail.com>
2014-11-24 18:08:44 -0200

    versao inicial

---
+++ src/main/java/br/univap/labs/jonas/controller/EquipamentosController.java
@@ -0,0 +1,20 @@
+/**
+ * @author rsaraiva
+ */
+package br.univap.labs.jonas.controller;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Path;
+
+@Controller
+public class EquipamentosController {
+
+    @Path("/equipamentos")
+    public void equipamentos() {
+        
+    }
+    
+    public void lista() {
+        
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/equipamentos/equipamentos.jsp
@@ -0,0 +1,11 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Equipamentos</title>
+    </head>
+    <body>
+        <h1>Equipamentos!</h1>
+    </body>
+</html>
